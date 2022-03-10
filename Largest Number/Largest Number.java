a643cb3375fa7149f81f3e44651559da1aa2a93c
Mukhammad <mukhammad9292@gmail.com>
2022-03-10 00:16:18 +0000

    Added comments, fixed guide

--- Largest Number/Largest Number.java
+++ Largest Number/Largest Number.java
@@ -1,15 +1,17 @@
 class Solution {
     public String largestNumber(int[] nums) {
         String[] str = new String[nums.length];
-        
+        // Transforming all ints into Strings and then storing in an array
         for (int i = 0; i < nums.length; i++) {
             str[i] = String.valueOf(nums[i]);                      
         }
         
+        // This part sorts Strings in array in order to form largest number
+        // Each number i compared to previous number j. Every time i is bigger than j, it is moved to the left
         for (int i = 0; i < str.length; i++) {
             String toInsert = str[i];
             int j = i;
-            
+            // Comparing and moving numbers. Compare method is used for comparison
             while (j > 0 && compare(toInsert, str[j - 1])) {
                 str[j] = str[j - 1];
                 j--;
@@ -18,9 +20,10 @@ class Solution {
         }
         
         StringBuilder finalStr = new StringBuilder();
-        
+        // This boolean used for edge case with all numbers being 0
         boolean allZeros = true;
         
+        // All array strings gathered in Stringbuilder and returned as a String
         for (int i = 0; i < str.length; i++) {
             finalStr.append(str[i]);
             if (!str[i].equals("0")) {

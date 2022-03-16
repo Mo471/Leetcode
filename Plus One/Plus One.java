cf051823459a63b5ca56d8a5e758a3da05886b05
Mukhammad <mukhammad9292@gmail.com>
2022-03-16 20:49:52 +0000

    New problem

---
+++ Plus One/Plus One.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        
+        for(int i = digits.length - 1; i >= 0; i--) {
+            
+            if(digits[i] < 9 ) {
+                
+                digits[i]++;
+                return digits;
+                
+            }
+            
+            else {
+                
+                digits[i] = 0;
+                
+            }
+                    
+        }
+    
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+        
+    }
+}
+
+/*
+[9,9,9,8]
+[8,9,9,8]
+[9,9,9,9]
+[8,9,9,9]
+[8,9,8,9,9]
+*/

787c722316656034a2b05b32676bfba7f2213a5c
Mukhammad <mukhammad9292@gmail.com>
2022-03-26 00:05:03 +0000

    Added guide + fixed formatting

--- Valid Perfect Square/Valid Perfect Square.java
+++ Valid Perfect Square/Valid Perfect Square.java
@@ -6,17 +6,18 @@ class Solution {
         int r = x/2;
         int mid;
         
-        while(l <= r) {
-            
+        while(l <= r) {            
             mid = (l + r) / 2;
             double squaredMid = (double)mid*mid; // double in case of int*int overflowing
             
-            if(squaredMid == x) return true;
-            else if(squaredMid > x) r = mid - 1;
-            else if(squaredMid < x) l = mid + 1;
-            
-        }
-        
+            if(squaredMid == x) {
+                return true;
+            } else if(squaredMid > x) {
+                r = mid - 1;
+            } else if(squaredMid < x) {
+                l = mid + 1;
+            }            
+        }        
         return false;
     }
 }
